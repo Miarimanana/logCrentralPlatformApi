41041d01953bff3abde91075e4f30ab3db2a1c01
LILA Mbolatiana <lila@lila.lila>
2026-04-29 21:56:06 +0300

    Fix: use environment variable for firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,24 @@
 const admin = require('firebase-admin');
 
+// if (!admin.apps.length) {
+//     admin.initializeApp({
+//         credential: admin.credential.cert(require('../../service-account.json')),
+//     });
+// }
+
+// module.exports = admin;const admin = require('firebase-admin');
+
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(require('../../service-account.json')),
-    });
+    try {
+        const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+        console.log("Firebase Admin initialisé avec succès.");
+    } catch (error) {
+        console.error("Erreur d'initialisation Firebase :", error.message);
+    }
 }
 
 module.exports = admin;
